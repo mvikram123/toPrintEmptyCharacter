c3094a2ad593f8d7f8108042abfb5d0f45d8e4f7
viccky8578 <manbodhvikram@gmail.com>
2023-03-09 12:25:35 +0530

    print empty Character

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        //char ch='';                   // it is not right way to print empty character.
-        char ch=Character.MIN_VALUE;    //this is the right way to print empty character.
+        //char ch1='';                   // it is not right way to assign empty character.
+       char ch=Character.MIN_VALUE;    //this is the right way to assign empty character.
         //or
-       // char ch='\u0000'; //this is the right way to print empty character.
-        //char ch='\0'; //this is the right way to print empty character.
+       // char ch='\u0000'; //this is the right way to assign  empty character.
+        //char ch='\0'; //this is the right way to assign empty character.
 
 
-        System.out.println("empty charcter!"+ch);
+        System.out.println("empty charcter."+ch);// output =empty character.
+       // System.out.println("print empty character"+ch1);//output error : empty character litral;
     }
 }
